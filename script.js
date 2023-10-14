339d54ec822811d47f31da9bb565f1c768cc7cc1
JAVID ASHWAQ F <112797329+Javid8048@users.noreply.github.com>
2023-10-14 09:05:20 +0000

    commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let currentDate = new Date();
 
 document.querySelector("#currentDate").innerHTML = `${currentDate.getDate()} / ${currentDate.getMonth() + 1} / ${currentDate.getFullYear()} `;
+
+$("body").ripples({
+    resolution: 512,
+	dropRadius: 20,
+	perturbance: 0.04,
+});
+
+$("body").ripples("play");
